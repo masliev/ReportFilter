a8d6b8540d0ecfd94010614ed8ded84b8e4ba167
ilya masliev <ilya.ma@ezscratch.com>
2018-03-13 23:13:43 +0300

    fix path

--- ReportFilter/__init__.py
+++ ReportFilter/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 app_settings = os.getenv(
     'APP_SETTINGS',
-    'project.server.config.DevelopmentConfig'
+    'ReportFilter.config.DevelopmentConfig'
 )
 app.config.from_object(app_settings)
 
