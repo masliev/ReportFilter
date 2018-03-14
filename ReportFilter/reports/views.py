44ae21709f498f8f55b8e61430f3e083f398f797
ilya masliev <ilya.ma@ezscratch.com>
2018-03-14 12:20:10 +0300

    adding date range controller

--- ReportFilter/reports/views.py
+++ ReportFilter/reports/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, make_response, jsonify
+from flask.views import MethodView
 
-from ReportFilter import app
-from ReportFilter.models import Report
+from ReportFilter import app, db
+from ReportFilter.models import Report, DateRange
 
 reports_blueprint = Blueprint('reports', __name__)
 
@@ -21,3 +22,39 @@ def contacts():
         json_results.append(data)
 
     return jsonify(items=json_results)
+
+
+class RangeAPI(MethodView):
+
+    def post(self):
+        post_data = request.get_json()
+
+        try:
+            d_range = DateRange(
+                start=post_data.get('start'),
+                end=post_data.get('end')
+            )
+
+            # insert the range
+            db.session.add(d_range)
+            db.session.commit()
+
+            responseObject = {
+                'status': 'success',
+                'message': 'Successfully added daterange.'
+            }
+            return make_response(jsonify(responseObject)), 201
+
+        except Exception as e:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Please choose another period.'
+            }
+            return make_response(jsonify(responseObject)), 401
+
+range_view = RangeAPI.as_view('range_api')
+reports_blueprint.add_url_rule(
+    '/reports/range',
+    view_func=range_view,
+    methods=['POST']
+)
